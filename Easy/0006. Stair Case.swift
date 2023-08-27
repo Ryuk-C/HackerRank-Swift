b3978d7da01693f7a3a40e5a5bb5250557339a09
Cuma Haznedar <78587220+Ryuk-C@users.noreply.github.com>
2023-08-27 14:50:02 +0200

    Create 0006. Stair Case.swift

---
+++ Easy/0006. Stair Case.swift
@@ -0,0 +1,32 @@
+class Solution {
+  
+// Created by Cuma Haznedar
+
+// Stair Case
+
+// Its base and height are both equal to . It is drawn using # symbols and spaces. The last line is not preceded by any spaces. Write a program that prints a staircase of size n.
+
+// Sample Input = 6
+
+// Sample Output
+
+//     #
+//    ##
+//   ###
+//  ####
+// #####
+//######
+
+func staircase(n: Int) -> Void {
+    
+    for i in 1...n {
+        
+        var space = String(repeating: " ", count: n - i)
+        var hash = String(repeating: "#", count: i)
+        
+        print(space + hash)
+        
+    }
+ }
+
+}
