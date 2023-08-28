815fee51c577f2c5194b75b6e59f27c0aa116c01
Cuma Haznedar <78587220+Ryuk-C@users.noreply.github.com>
2023-08-28 17:13:56 +0200

    Update 0007. Mini-Max Sum.swift

--- Easy/0007. Mini-Max Sum.swift
+++ Easy/0007. Mini-Max Sum.swift
@@ -3,14 +3,15 @@ class Solution {
 // Created by Cuma Haznedar
 
 // Mini-Max Sum
-
+// https://www.hackerrank.com/challenges/mini-max-sum/problem
+  
 // Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. 
 // Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 
-//Output Format
+// Output Format
 
-//Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. 
-//(The output can be greater than a 32 bit integer.)
+// Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. 
+// The output can be greater than a 32 bit integer.
 
   func miniMaxSum(arr: [Int]) -> Void {
     
